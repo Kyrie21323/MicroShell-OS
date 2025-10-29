2ee036218504a599293a25e79c8e7e0a3572bbd6
Nikita Gupta <123475875+Niki1311@users.noreply.github.com>
2025-10-29 20:42:28 +0400

    Update server.c

--- src/server.c
+++ src/server.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <errno.h>
+#include <stdbool.h>
 
 #define MAX_CMD_LENGTH 1024 
 #define MAX_ARGS 64         
@@ -24,6 +25,7 @@ void sigint_handler(int sig){
     printf("\n[INFO] SIGINT received, stopping...\n");
     g_stop = 1;
     if(server_fd >= 0){
+        // This helps unblock the accept() call
         shutdown(server_fd, SHUT_RDWR);
     }
 }
@@ -66,10 +68,11 @@ int main(int argc, char *argv[]) {
         client_fd = accept_client_connection(server_fd);
         if(client_fd < 0){
             if(g_stop){
-                printf("[INFO] Shutting down server...\n");
+                // Normal shutdown path
                 break;
             }
             if(errno == EINTR){
+                // Interrupted by signal, loop will check g_stop
                 continue;
             }
             fprintf(stderr, "Error: Failed to accept client connection\n");
@@ -78,10 +81,8 @@ int main(int argc, char *argv[]) {
 
         printf("[INFO] Client connected.\n");
 
-        while(1){
-            if(g_stop){
-                break;
-            }
+        // The Fix: This loop now checks the g_stop flag
+        while(!g_stop){
             
             int bytes_received = receive_line(client_fd, cmd_buffer, sizeof(cmd_buffer));
             
@@ -90,6 +91,7 @@ int main(int argc, char *argv[]) {
                     printf("[INFO] Client disconnected.\n");
                 } else {
                     if(errno == EINTR){
+                        // Interrupted by signal, loop will check g_stop
                         continue;
                     }
                     perror("Error receiving command");
@@ -104,13 +106,14 @@ int main(int argc, char *argv[]) {
                 break;
             }
             if(strlen(cmd_buffer) == 0){
+                // Send back an empty response for an empty command
+                send_line(client_fd, "");
                 continue;
             }
             
             printf("[EXECUTING] Executing command: \"%s\"\n", cmd_buffer);
 
             char *output_str = NULL;
-            // Use a copy for parsing/execution because strtok_r in execute_pipeline modifies it
             char cmd_copy[MAX_CMD_LENGTH];
             strncpy(cmd_copy, cmd_buffer, MAX_CMD_LENGTH);
 
@@ -119,7 +122,8 @@ int main(int argc, char *argv[]) {
             } else {
                 char *args[MAX_ARGS];
                 char *inputFile, *outputFile, *errorFile;
-                if(parse_command(cmd_copy, args, &inputFile, &outputFile, &errorFile, 0) == 0){
+                int parse_res = parse_command(cmd_copy, args, &inputFile, &outputFile, &errorFile, 0);
+                if(parse_res == PARSE_SUCCESS){
                     output_str = execute_command(args, inputFile, outputFile, errorFile);
                     
                     for(int i = 0; args[i] != NULL; i++) free(args[i]);
@@ -127,27 +131,53 @@ int main(int argc, char *argv[]) {
                     if(outputFile) free(outputFile);
                     if(errorFile) free(errorFile);
                 } else {
-                    output_str = xstrdup("Error: Command parsing failed.\n");
+                     switch (parse_res) {
+                        case PARSE_ERR_SYNTAX:
+                        case PARSE_ERR_EMPTY_CMD_REDIR:
+                            output_str = xstrdup("Error: Invalid command syntax.\n"); break;
+                        case PARSE_ERR_TOO_MANY_ARGS:
+                            output_str = xstrdup("Error: Too many arguments.\n"); break;
+                        case PARSE_ERR_NO_INPUT_FILE:
+                            output_str = xstrdup("Input file not specified.\n"); break;
+                        case PARSE_ERR_NO_OUTPUT_FILE:
+                            output_str = xstrdup("Output file not specified.\n"); break;
+                        case PARSE_ERR_NO_ERROR_FILE:
+                            output_str = xstrdup("Error output file not specified.\n"); break;
+                        default:
+                            output_str = xstrdup("Error: Command parsing failed.\n");
+                    }
                 }
             }
             
-            // Log output before sending to client, as per project requirements
-            if (output_str && strlen(output_str) > 0) {
-                 // Check if the last character is a newline, if not, the formatting could be messy
-                int len = strlen(output_str);
+            bool is_error = false;
+            if (output_str) {
+                // Better error checking
+                if (strstr(output_str, "not found") || strstr(output_str, "not specified") ||
+                    strstr(output_str, "Invalid") || strstr(output_str, "failed") ||
+                    strstr(output_str, "Error:")) {
+                    is_error = true;
+                }
+            }
+
+            if (is_error) {
+                char log_msg[MAX_CMD_LENGTH];
+                strncpy(log_msg, output_str, sizeof(log_msg) - 1);
+                log_msg[sizeof(log_msg) - 1] = '\0';
+                // Remove trailing newline for logging
+                log_msg[strcspn(log_msg, "\n")] = 0; 
+                printf("[ERROR] %s\n", log_msg);
+                printf("[OUTPUT] Sending error message to client: \"%s\"\n", log_msg);
+            } else if (output_str && strlen(output_str) > 0) {
+                 int len = strlen(output_str);
                 if (output_str[len - 1] == '\n') {
                     printf("[OUTPUT] Sending output to client:\n%s", output_str);
                 } else {
                     printf("[OUTPUT] Sending output to client:\n%s\n", output_str);
                 }
             } else {
-                 // Empty output is normal for commands with redirections (e.g., echo hi > file.txt)
                  printf("[OUTPUT] Sending empty output to client.\n");
             }
 
-
-            // Send the result back to the client (always send, even if empty)
-            // This ensures the client doesn't hang waiting for a response
             if (send_line(client_fd, output_str ? output_str : "") < 0) {
                 perror("Error sending output to client");
             }
@@ -160,16 +190,8 @@ int main(int argc, char *argv[]) {
         close_socket(client_fd);
         client_fd = -1;
         printf("[INFO] Client session ended.\n");
-        
-        if(g_stop){
-            printf("[INFO] Shutting down server...\n");
-            break;
-        }
     }
 
-    if(client_fd >= 0){
-        close_socket(client_fd);
-    }
     if(server_fd >= 0){
         close_socket(server_fd);
     }
