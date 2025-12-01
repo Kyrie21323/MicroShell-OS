eb58f4febf8253c67f1069efbf98fb1a44bee14e
Nikita Gupta <123475875+Niki1311@users.noreply.github.com>
2025-12-01 09:39:42 +0530

    Update parse.h

--- include/parse.h
+++ include/parse.h
@@ -1,7 +1,5 @@
 #ifndef PARSE_H
 #define PARSE_H
-
-// Return codes for parse_command
 #define PARSE_SUCCESS 0
 #define PARSE_ERR_SYNTAX 1
 #define PARSE_ERR_TOO_MANY_ARGS 2
@@ -11,14 +9,10 @@
 #define PARSE_ERR_NO_ERROR_FILE 5
 #define PARSE_ERR_EMPTY_CMD_REDIR 6
 #define PARSE_ERR_UNCLOSED_QUOTES 7
-
-// Return codes for validate_pipeline
 #define VALIDATE_SUCCESS 0
 #define VALIDATE_ERR_STARTS_PIPE 1
 #define VALIDATE_ERR_EMPTY_CMD 2
 #define VALIDATE_ERR_ENDS_PIPE 3
-
 int parse_command(char *cmd, char *args[], char **inputFile, char **outputFile, char **errorFile, int isPipeline, int *outputAppend);
 int validate_pipeline(char *cmd);
-
 #endif
